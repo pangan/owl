6aaced0ed59970c45eceee3e308bc9917a14eb73
root <pangan@gmail.com>
2023-04-01 20:20:22 +0100

    more develop

--- src/main.py
+++ src/main.py
@@ -35,5 +35,32 @@ def good():
 	while True:
 		pass
 
+
+def foo():
+	signal.signal(signal.SIGINT, gr_exit)
+	global all_processes
+	bs = '../../debug/.venv/bin/'
+	cms = [f'{bs}debin', f'{bs}exit_code', f'{bs}make_exception']
+
+	for cc in cms:
+		child = subprocess.Popen(cc, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		all_processes.append(child)
+
+	print('Running')
+	while True:
+		time.sleep(1)
+		new_pr_list = []
+		for cc in all_processes:
+			error_code = cc.poll()
+			# output, error_code = cc.communicate()
+			if error_code:
+				print(f'---->>> {cc} exit: {error_code}, output = {cc.stderr.read()}')
+				cc.terminate()
+			else:
+				new_pr_list.append(cc)
+		all_processes = new_pr_list
+
+
+
 if __name__ == '__main__':
-	good()
+	foo()
